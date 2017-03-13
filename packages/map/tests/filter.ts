e534874ddc7a54109cf13ee44ef2c62c9606ee6f
Nathan Ridley <axefrog@gmail.com>
2017-03-14 04:37:39 +1000

    MAP(test): Implemented test for filter()

---
+++ packages/map/tests/filter.ts
@@ -0,0 +1,32 @@
+import {assert} from 'chai';
+import {fromArray, filter} from '../src';
+
+type Pair = [number, string];
+function pairFromNumber(n: number): [number, string] {
+  return [n, `#${n}`];
+}
+
+suite('[Map]', () => {
+  suite('filter()', () => {
+    let values0: Pair[], values1: Pair[], values2: Pair[];
+    const predicate1 = (value: string, key: number) => ((key >>> 2) << 2) !== key;
+    const predicate2 = (value: string, key: number) => ((key >>> 2) << 2) === key;
+    suiteSetup(() => {
+      values0 = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89].map(pairFromNumber);
+      values1 = values0.filter(v => predicate1(v[1], v[0]));
+      values2 = values0.filter(v => predicate2(v[1], v[0]));
+    });
+
+    test('items are considered excluded if the predicate returns a falsey value', () => {
+      const map0 = fromArray(values0);
+      const map1 = filter(predicate1, map0);
+      assert.sameDeepMembers(Array.from(map1), values1);
+    });
+
+    test('items are considered included if the predicate returns a truthy value', () => {
+      const map0 = fromArray(values0);
+      const map1 = filter(predicate2, map0);
+      assert.sameDeepMembers(Array.from(map1), values2);
+    });
+  });
+});
