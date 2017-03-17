4c752df1d9d89d63b13782a06de689bbaed80d5b
Nathan Ridley <axefrog@gmail.com>
2017-03-18 04:16:17 +1000

    MAP(impl): Add mutable option to empty()

--- packages/map/src/functions/empty.ts
+++ packages/map/src/functions/empty.ts
@@ -1,11 +1,14 @@
+import {batch, nextId} from '@collectable/core';
 import {isDefined} from '@collectable/core';
 import {EMPTY} from '../internals/nodes';
 import {HashMap, HashMapImpl, isHashMap} from '../internals/HashMap';
 
 var EMPTY_MAP: HashMapImpl<any, any>;
 
-export function empty<K, V>(): HashMap<K, V> {
-  return isDefined(EMPTY_MAP) ? EMPTY_MAP : (EMPTY_MAP = new HashMapImpl<any, any>(0, 0, EMPTY, 0));
+export function empty<K, V>(mutable = false): HashMap<K, V> {
+  return mutable
+    ? new HashMapImpl<any, any>(batch.owner(true), nextId(), EMPTY, 0)
+    : isDefined(EMPTY_MAP) ? EMPTY_MAP : (EMPTY_MAP = new HashMapImpl<any, any>(0, 0, EMPTY, 0));
 }
 
 export function isMap<K, V>(arg: any): arg is HashMap<K, V> {
