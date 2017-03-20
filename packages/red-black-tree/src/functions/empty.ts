28da9ad272ee03d07cd98c6905db0b169b240026
Nathan Ridley <axefrog@gmail.com>
2017-03-20 18:53:46 +1000

    RBT(impl): Optional mutable parameter for empty()

--- packages/red-black-tree/src/functions/empty.ts
+++ packages/red-black-tree/src/functions/empty.ts
@@ -7,13 +7,20 @@ import {RedBlackTree, ComparatorFn, createTree, isRedBlackTree as _isRedBlackTre
  * @export
  * @template K The type of keys in the tree
  * @template V The type of values in the tree
- * @param {ComparatorFn<K>} [ComparatorFn] A comparison function, taking two keys, and returning a value less than 0 if the
- *                                     first key is smaller than the second, a value greater than 0 if the first key is
- *                                     greater than the second, or 0 if they're the same.
+ * @param {boolean} [mutable] If true, the returned tree will be returned in a mutable state
+ * @param {ComparatorFn<K>} [compare] A comparison function, taking two keys, and returning a value less than 0 if the
+ *                                    first key is smaller than the second, a value greater than 0 if the first key is
+ *                                    greater than the second, or 0 if they're the same.
  * @returns {RedBlackTree<K, V>} An empty tree
  */
-export function empty<K, V>(ComparatorFn?: ComparatorFn<K>): RedBlackTree<K, V> {
-  return createTree<K, V>(false, ComparatorFn);
+export function empty<K, V>(compare?: ComparatorFn<K>): RedBlackTree<K, V>;
+export function empty<K, V>(mutable: boolean, compare?: ComparatorFn<K>): RedBlackTree<K, V>;
+export function empty<K, V>(mutable?: boolean|ComparatorFn<K>, compare?: ComparatorFn<K>): RedBlackTree<K, V> {
+  if(typeof mutable === 'function') {
+    compare = mutable;
+    mutable = false;
+  }
+  return createTree<K, V>(mutable || false, compare);
 }
 
 /**
