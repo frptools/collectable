47e922469870a7267996946cdc43189d72824947
Nathan Ridley <axefrog@gmail.com>
2017-07-31 09:47:17 +1000

    MAIN(build): Remove default params from gulp script

--- scripts/lib/gulp.js
+++ scripts/lib/gulp.js
@@ -94,7 +94,7 @@ function createGulpEnv(pkgname) {
   function clean() {
     const paths = [`${path}/lib`, `${path}/.build`];
     return function(cb) {
-      const next = (i = 0) => rimraf(paths[i], done(i + 1));
+      const next = (i) => rimraf(paths[(i || 0)], done((i || 0) + 1));
       const done = i => err => err ? cb(err) : i === paths.length ? cb() : next(i);
       return next();
     }
