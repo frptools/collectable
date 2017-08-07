a7c6c9663e4bf9a4f9befbf9f8dcfa7c4a1c3cab
joefraley <jose.fraley@gmail.com>
2017-08-06 21:22:03 -0700

    MAIN(fix): TypeError when using setIn() to set a deeply nested value, fixes #55 - Includes guard that prevents isIndexedCollection() from running against primitive values - Includes test coverage to prove deep nested writing and overwriting function

--- src/functions/set.ts
+++ src/functions/set.ts
@@ -1,6 +1,9 @@
 import {IndexedCollection, isIndexedCollection} from '@collectable/core';
 import {fromArray} from '@collectable/map';
 
+// Intended to guard isIndexedCollection from testing primitive values
+const isPrimitive = (value) => Object(value) !== value;
+
 export function setIn<K, V>(path: any[], value: any, collection: IndexedCollection<K, V>): IndexedCollection<K, V> {
   collection = setDeep(collection, path, 0, value);
   return collection;
@@ -8,7 +11,7 @@ export function setIn<K, V>(path: any[], value: any, collection: IndexedCollecti
 
 function setDeep(collection: IndexedCollection<any, any>, path: any[], keyidx: number, value: any): IndexedCollection<any, any> {
   var key = path[keyidx];
-  if(isIndexedCollection(collection) && IndexedCollection.verifyKey(key, collection)) {
+  if (!isPrimitive(collection) && isIndexedCollection(collection) && IndexedCollection.verifyKey(key, collection)) {
     return keyidx === path.length - 1
       ? IndexedCollection.set(key, value, collection)
       : IndexedCollection.updateEntry((c: any) => setDeep(c, path, keyidx + 1, value), key, collection);
